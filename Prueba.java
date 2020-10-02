f9be109d9c294f1dee632e32f3df3701f074accc
Roberto <robertoab28@gmail.com>
2020-10-02 17:21:23 +0200

    Escribe donde vivo y mi sistema operativo

--- Prueba.java
+++ Prueba.java
@@ -1,6 +1,7 @@
 public class Prueba{
     public static void main(String args[]){
-        System.out.println("Leon - 1996")
+        System.out.println("Leon - 1996");
         System.out.println("Roberto Alarcon Bardon");
+        System.out.println("Carbajal de la Legua - Windows 10");
     }
 }
